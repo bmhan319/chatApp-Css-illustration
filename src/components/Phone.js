8611e3b5c8dd646c998a131e8c611a0a6b555c18
Brian Han <brianhan@brians-mbp.lan>
2020-12-02 00:13:02 -0800

    phone header complete

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import avatar from '../img/avatar.jpg'
 
 export default function Phone() {
   return (
@@ -7,16 +8,18 @@ export default function Phone() {
         <div className="phoneHeader">
           <div className="headerCutout"></div>
           <div className="headerContainer">
-            <div className="headerBack"></div>
-            <div className="headerImg"></div>
+            <div className="headerBack">{"<"}</div>
+            <div className="headerImgContainer">
+              <img className="headerImg" src={avatar} alt='avatar' />
+            </div>
             <div className="headerTextContainer">
               <h3 className="headerName">Samuel Green</h3>
               <p className="headerText">Available to Walk</p>
             </div>
             <div className="headerMenu">
-              <span className="burgerTop">.</span>
-              <span className="burgerMid">.</span>
-              <span className="burgerBot">.</span>
+              <span className="burger burgerTop"></span>
+              <span className="burger burgerMid"></span>
+              <span className="burger burgerBot"></span>
             </div>
           </div>
         </div>
