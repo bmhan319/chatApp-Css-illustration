cac6a872328d3268a274fc4e228149f430acca40
Brian Han <brianhan@brians-mbp.lan>
2020-12-02 01:59:49 -0800

    message component complete

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Message() {
   return (
-    <div>
-      
+    <div className="messageComponent">
+      <div className="messageContainer">
+        <h1 className="messageHeadline">Simple booking</h1>
+        <p className="messageParagraph">Stay in touch with our dog walkers through the chat interface. This makes it easy to 
+  discuss arrangements and make bookings. Once the walk has been completed you can rate 
+  your walker and book again all through the chat.</p>
+      </div>
     </div>
   )
 }
